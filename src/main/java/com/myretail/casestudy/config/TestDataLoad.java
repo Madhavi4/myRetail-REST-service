a623017eeae9c129bf72c44d47de344248578ed4
Madhavi4 <madhavid30@gmail.com>
2019-12-01 22:51:33 -0600

    add logs

--- src/main/java/com/myretail/casestudy/config/TestDataLoad.java
+++ src/main/java/com/myretail/casestudy/config/TestDataLoad.java
@@ -2,12 +2,14 @@ package com.myretail.casestudy.config;
 
 import com.myretail.casestudy.model.ProductPrice;
 import com.myretail.casestudy.repository.ProductPriceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
+@Slf4j
 public class TestDataLoad {
 
     @Autowired
@@ -15,11 +17,13 @@ public class TestDataLoad {
 
     @PostConstruct
     public void loadTestPriceDetails() {
+        log.info("Loading test data!!!");
         ProductPrice productPrice1 = new ProductPrice(54498967L, 57.99, "USD");
         ProductPrice productPrice2 = new ProductPrice(76780149L, 59.99, "USD");
         ProductPrice productPrice3 = new ProductPrice(76593324L, 79.99, "USD");
         productPriceRepository.save(productPrice1);
         productPriceRepository.save(productPrice2);
         productPriceRepository.save(productPrice3);
+        log.info("Test data load complete!!!");
     }
 }
